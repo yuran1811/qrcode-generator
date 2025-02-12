5e8606c8617c17bcea17c08713f7fc9c1a748350
yuran1811 <trieuvanbd123@gmail.com>
2025-02-12 20:30:39 +0700

    feat: ✨ upgrade tailwind v4 and other dependencies to latest version

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,5 @@
-module.exports={plugins:{tailwindcss:{},autoprefixer:{}}}
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
--- tailwind.config.js
+++
@@ -1,1 +0,0 @@
-module.exports={mode:'jit',content:['./index.html','./src/**/*.{vue,js,ts,jsx,tsx}']}
